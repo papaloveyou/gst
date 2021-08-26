19860c84e5cbd11848340fdf9cb650b04f732da3
papaloveyou <papaloveyou@mail.com>
2021-08-27 02:44:28 +0800

    new

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 	"time"
 )
 
-const MV = "gsutil mv %v%v %v"
+const MV = "gsutil mv %v%v gs://%v"
 
 //var m = make(map[string]string)
 var path, bucket, suffix string
@@ -23,19 +23,25 @@ var bytes int64
 func main() {
 	app := app.New()
 	app.Action = func(c *cli.Context) (err error) {
-		if c.NArg() < 2 {
+		if c.NArg() != 2 {
 			fmt.Fprintf(os.Stderr, "Error: %s takes arguments error.\n\n", app.Name)
+			cli.ShowAppHelp(c)
+			os.Exit(1)
+		}
+
+		if !strings.HasSuffix(c.Args().First(), string(os.PathSeparator)) {
+			fmt.Printf("Wrong path.\n\n")
 			cli.ShowAppHelp(c)
 			os.Exit(1)
 		}
 
 		path, _ = filepath.Split(c.Args().First())
 		//path, _ = filepath.Split("/root/qq/")
-		suffix := c.String("ext")
+		suffix = strings.ToUpper(c.String("ext"))
 		bytes = utils.ParseSize(c.String("size"))
 		bucket = c.Args().Get(1)
 		mtime := c.Uint("time")
-		var i uint = 1
+		var i uint = mtime
 		fmt.Printf("path: %v bucket: %v suffix: %v size: %v  time: %v\n\n", path, bucket, suffix, bytes, mtime)
 		//fmt.Print(getExecRet("ps -ef"))
 		for {
@@ -51,14 +57,16 @@ func main() {
 
 				for _, file := range files {
 					if !file.IsDir() {
-						if file.Size() >= bytes && strings.HasSuffix(file.Name(), suffix) {
+						if file.Size() >= bytes && strings.HasSuffix(strings.ToUpper(file.Name()), suffix) {
 							if isProcessing(file.Name()) {
 								continue
 							}
+							//fmt.Printf("%v Start to transfer files %v\n", time.Now().Format("2006-01-02 15:04:05"), path+file.Name())
 							go work(file.Name())
 						}
 					}
 				}
+				fmt.Printf("Perform the next scan after %d minutes.\n", mtime)
 				i = 1
 				time.Sleep(time.Minute)
 			}
@@ -88,7 +96,8 @@ func isProcessing(filename string) bool {
 func work(filename string) {
 	x := fmt.Sprintf(MV, path, filename, bucket)
 
-	println("=>", filename)
+	//println("=>", filename)
+	fmt.Printf("%v Start to transfer files %v\n", time.Now().Format("2006-01-02 15:04:05"), path+filename)
 	cmd := exec.Command("sh", "-c", x)
 	if err := cmd.Run(); err != nil {
 		println(err.Error())
